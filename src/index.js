56e9540417a4e14aae8c8f68dea94d36a06c3eab
Hossein Behzad <behzadhossein916@gmail.com>
2022-04-12 12:32:20 +0430

    updated index.js file and added some methods

--- src/index.js
+++ src/index.js
@@ -2,6 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import axios from "axios";
+
+// default globals
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+axios.defaults.headers.post["Content-type"] = "application/json";
+axios.defaults.headers.common["Authorization"] = "Bearer Token";
+
+// interceptors in Axios
+//  request
+axios.interceptors.request.use(
+  (request) => {
+    console.log(request);
+    return request;
+  },
+  // for error handling
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+//  response
+axios.interceptors.response.use(
+  (response) => {
+    console.log(response);
+    return response.data;
+  },
+  // for error handling
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // const childRoot = ReactDOM.createRoot(document.getElementById('child-root'));
